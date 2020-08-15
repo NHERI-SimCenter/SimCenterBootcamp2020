db33604e19b79bb25c0f0d3109f0f0a024ecb126
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2020-08-15 11:04:58 -0700

    Adding Peter's solution for pi

---
+++ code/PetersCode/pi/pi-omp.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "omp.h"
+
+/* ******************** *
+ * set default values   *
+ * ******************** */
+#define N  851558400
+//#define N  (2*3*4*5*6*7*8)
+#define NUM_THREADS 8
+
+
+int main(int argc, char **argv) {
+
+    // initialize an internal timer
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
+    // set up thread number from defaults and potential user input
+
+    int num_threads = NUM_THREADS;
+
+    if (argc >=2) {
+	num_threads = atoi(argv[1]);
+    }
+    if (num_threads < 1) num_threads = 1;
+    if (num_threads > 8) num_threads = 8;
+
+    int n=N/num_threads;
+
+    // configure parallel arrays and workflow
+
+    omp_set_dynamic(0);
+    omp_set_num_threads(num_threads);
+
+    double dx = 1./(double)n/(double)num_threads;
+
+    double pi=0.0;
+
+#pragma omp parallel
+{
+    int ID=omp_get_thread_num();
+
+    //printf("starting thread %d\n",ID);
+
+    double s = 0.;
+
+    double x = (double)ID / num_threads;
+    x += 0.5*dx;
+
+    for (int i=0; i<n; i++,x+=dx) {
+	s += 4./(1.+x*x);
+    }
+    printf("... (%d): %20.16f\n",ID,s);
+
+#pragma omp atomic
+    pi += s;
+}
+
+    pi *= dx ;
+
+    // check end time
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    // nice printing the result
+
+    printf("pi = %20.16f (%10.6f secs)\n", pi, cpu_time_used);
+
+    return 0;
+}
+
---
+++ code/PetersCode/pi/pi.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+/* ******************** *
+ * set default values   *
+ * ******************** */
+#define N  851558400
+
+
+int main(int argc, char **argv) {
+
+    // initialize an internal timer
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
+    // set up thread number from defaults and potential user input
+
+    int n=N;
+
+    if (argc >=2) {
+	n = atoi(argv[1]);
+	if (n < 100) n = 100;
+    }
+
+    double pi=0.0;
+
+    double dx = 1./n;
+    double x = 0.5*dx;
+
+    for (int i=0; i<n; i++,x+=dx) {
+	pi += 4./(1.+x*x);
+    }
+
+    pi *= dx ;
+
+    // check end time
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    // nice printing the result
+
+    printf("pi = %20.16f (%10.6f secs)\n", pi, cpu_time_used);
+
+    return 0;
+}
+
