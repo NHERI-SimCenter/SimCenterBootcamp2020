8904b3ed5975783c9300431066c2767310df5266
fmckenna <fmckenna@berkeley.edu>
2021-01-07 09:48:10 -0800

    fmk - updating bash script

---
+++ code/parallel/ExerciseDay4/ex2/submitPI.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#--------------------------------------------------------------------
+# Generic SLURM script – MPI Hello World
+#
+# This script requests 1 node and 8 cores/node (out of total 64 avail)
+# for a total of 1*8 = 8 MPI tasks.
+#---------------------------------------------------------------------
+#SBATCH -J myjob
+#SBATCH -o myjob.%j.out 
+#SBATCH -e myjob.%j.err 
+#SBATCH -p development
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 00:02:00
+#SBATCH -A DesignSafe-SimCenter
+
+echo "PI1"
+export OMP_NUM_THREADS=1
+./pi
+export OMP_NUM_THREADS=2
+./pi
+export OMP_NUM_THREADS=4
+./pi
+export OMP_NUM_THREADS=8
+./pi
+
+
