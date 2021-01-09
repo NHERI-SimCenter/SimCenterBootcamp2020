680214ace3b4c1fcfc08f47b7034bc3ed60038e9
fmckenna <fmckenna@berkeley.edu>
2021-01-09 07:47:37 -0800

    fmk - updates for assignmentC5

---
+++ sphynx/source/solutions/c5/wrapper.sh
@@ -0,0 +1,26 @@
+
+set -x
+WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
+
+${AGAVE_JOB_CALLBACK_RUNNING}
+
+module load intel
+
+echo "program file is ${programFile}"
+pwd
+ls -sal
+cd "${inputDirectory}"
+echo "currentDIR"
+pwd
+echo "directory listing"
+ls -sal
+
+mpicc ${programFile}
+ibrun ./a.out
+rm ./a.out
+
+if [ ! $? ]; then
+        echo "program exited with an error status. $?" >&2
+        ${AGAVE_JOB_CALLBACK_FAILURE}
+        exit
+fi
